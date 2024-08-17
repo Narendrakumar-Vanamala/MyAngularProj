c1fdfd6478a0f4f7b14744aa056fe996f30cf879
Narendrakumar <v.narendranaga@gmail.com>
2024-08-17 21:47:12 +0530

    added space

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,7 +8,6 @@ import { Label } from 'ng2-charts';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent implements OnInit{
-
   public barChartOptions: ChartOptions = {
     responsive: true,
     scales: { x: { stacked: true }, y: { stacked: true } }
