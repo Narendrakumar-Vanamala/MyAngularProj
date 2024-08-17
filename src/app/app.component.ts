ca8bd18760c4daaa0142eb479bb87090824617cd
Narendrakumar <v.narendranaga@gmail.com>
2024-08-17 21:57:04 +0530

    test commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,6 +8,8 @@ import { Label } from 'ng2-charts';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent implements OnInit{
+
+
   public barChartOptions: ChartOptions = {
     responsive: true,
     scales: { x: { stacked: true }, y: { stacked: true } }
